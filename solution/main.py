b06e1c7875641891c80a7ec4411903feccd21dca
Gonzalo Rubio <gonzrubio95@gmail.com>
2024-04-29 17:49:52 -0700

    use wandb logger

--- solution/main.py
+++ solution/main.py
@@ -3,6 +3,7 @@ import time
 import hydra
 import lightning as pl
 import torch
+from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from model import EarthQuakeModel
 from omegaconf import DictConfig
@@ -11,28 +12,34 @@ from torchgeo.datamodules import QuakeSetDataModule
 
 @hydra.main(config_path="configs", config_name="default", version_base=None)
 def main(args: DictConfig):
+
     pl.seed_everything(42, workers=True)
     torch.set_float32_matmul_precision("medium")
 
     data_module = QuakeSetDataModule(**args.dataset)
     model = EarthQuakeModel(**args.model)
 
-    experiment_id = time.strftime("%Y%m%d-%H%M%S")
+    run_id = time.strftime('%Y%m%d-%H%M%S')
+    wandb_logger = WandbLogger(
+        project="smac",
+        name=run_id,
+        log_model="all"
+        )
 
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
-        dirpath=f"checkpoints/{experiment_id}",
+        dirpath=f"checkpoints/{run_id}",
         filename="earthquake-detection-{epoch:02d}-{val_loss:.2f}",
         save_top_k=3,
         mode="min",
     )
-
     lr_monitor = LearningRateMonitor(logging_interval="step")
 
     trainer = pl.Trainer(
         **args.trainer,
         deterministic=True,
         callbacks=[checkpoint_callback, lr_monitor],
+        logger=wandb_logger,
         log_every_n_steps=50,
         precision="32-true",
     )
