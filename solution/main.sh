cb748d33181c85dae944d16efbd3ec115dfa8393
Gonzalo Rubio <gonzrubio95@gmail.com>
2024-05-15 17:15:39 -0700

    sweep over class loss weights

--- solution/main.sh
+++ solution/main.sh
@@ -12,13 +12,13 @@ REGRESSION_LOSS_COEFFICIENTS_LIST=(
 )
 
 CLASSIFICATION_LOSS_COEFFICIENT_LIST=(
-    "1e-3"
-    "1e-2"
-    "1e-1"
-    "1"
+    0.001
+    0.01
+    0.1
+    0.0001
 )
 
-for CLASSIFICATION_LOSS_COEFFICIENT in "${CLASSIFICATION_LOSS_COEFFICIENT_LIST[@]}"
+for CLASSIFICATION_LOSS_COEFFICIENT in 0.001 0.01 0.1 1.0
 do
-    python main.py trainer.max_epochs=$MAX_EPOCHS model.regression_loss="$REGRESSION_LOSS" model.classification_loss_coefficient="$CLASSIFICATION_LOSS_COEFFICIENT"
+    python main.py trainer.max_epochs=$MAX_EPOCHS model.regression_loss="$REGRESSION_LOSS" model.classification_loss_coefficient=$CLASSIFICATION_LOSS_COEFFICIENT
 done
